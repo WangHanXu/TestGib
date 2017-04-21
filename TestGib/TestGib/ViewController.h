0de5998388029f496c50acdeafe757e99f2764e6
Mac os x <macosx@Macs-MacBook-Pro.local>
2017-04-21 12:03:32 +0800

    Test Git Hub

---
+++ TestGib/TestGib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGib
+//
+//  Created by Mac os x on 2017/4/21.
+//  Copyright © 2017年 Mac os x. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestGib/TestGibTests/TestGibTests.m
@@ -0,0 +1,39 @@
+//
+//  TestGibTests.m
+//  TestGibTests
+//
+//  Created by Mac os x on 2017/4/21.
+//  Copyright © 2017年 Mac os x. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestGibTests : XCTestCase
+
+@end
+
+@implementation TestGibTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
